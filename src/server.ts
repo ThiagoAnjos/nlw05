bf56960c65b10ec1cff5d4522ce4e742567415d5
Thiago Anjos <tafas.anjos@gmail.com>
2021-04-20 23:49:09 -0300

    Configurações iniciais

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import "./database";
+
+const app = express();
+
+app.listen(3333, () => console.log("Server is running on port 3333"));
+
+/***
+ * GET => BUSCA
+ * POST => CRIACAO
+ * PUT => ALTERAR
+ * DELETE => DELETAR
+ * PATCH => ALTERAR CAMPO ESPECIFICO
+ */
+
+app.get("/", (request, response) => {
+  return response.json({message: "Olá NLW5!"})
+});
+
+app.post("/", (request, response) => {
+  return response.json({message: "Usuário cadastrado com sucesso!"})
+});
+
+app.delete("/", (request, response) => {
+  return response.json({message: "Usuário excluído com sucesso!"})
+});
+
+app.put("/", (request, response) => {
+  return response.json({message: "Usuário alterado com sucesso!"})
+});
+
+app.patch("/", (request, response) => {
+  return response.json({message: "Usuário atualizado com sucesso!"})
+});
